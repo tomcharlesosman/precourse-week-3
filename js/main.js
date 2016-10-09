777b96238ee2882dc1d63758b3b3c2fc868b9d52
tomcharlesosman <tomcharlesosman@gmail.com>
2016-10-09 23:26:53 +0100

    Committing all files for Week 3 challenge :)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-     $("h1").click(function () {
-          console.log("clicked");
+     $('h1').click(function () {
+          console.log('clicked')
      });
 });
+
+$('.snapchat').mouseenter(function () {
+     $('.snapchat').addClass('animated hinge 1');
+});
+
+$('.fadeInText').addClass('animated bounceInDown');
